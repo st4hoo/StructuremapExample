0d3232518012fb165c9ebe960dd1b89c9892d109
st4hoo <stasiak.pawel@gmail.com>
2014-05-12 13:29:45 +0200

    SM example initial version

---
+++ StructuremapAutomap/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+using StructureMap;
+
+namespace StructuremapAutomap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectFactory.Initialize(x => 
+                { x.Scan(
+                      scan =>
+                      {
+                          scan.AssembliesFromPath(Environment.CurrentDirectory + @"\Impl" );
+                          scan.WithDefaultConventions();
+                      }
+                    );
+                });
+            
+            Console.WriteLine(ObjectFactory.WhatDoIHave());
+
+            Console.ReadLine();
+        }
+    }
+}
